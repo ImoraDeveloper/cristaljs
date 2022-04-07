62bc0dc12d050419f720a8a5aaa9103a159cd217
ImoraDeveloper <moraivan15a@gmail.com>
2022-04-07 19:36:02 -0300

    añadiendo totales

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const producto3 = new Postres("torta de oreo", 400);
 
 const producto4 = new Postres("torta 3 leches", 400);
 
-let postresElegidos = [producto2, producto4];
+let postresElegidos = [producto1, producto2, producto3, producto4];
 
 let nombre = prompt("Ingrese su nombre");
 let apellido = prompt("ingrese su apellido");
@@ -29,7 +29,7 @@ let ingresado = prompt(
   `Hola ${nombre}, Bienvenido a Cristal Cake elige tus postres:
   Marque 1 para ${producto1.articulo} ${producto1.precio}
   Marque 2 para ${producto2.articulo} ${producto2.precio}
-  Marque 3 para ${producto3.articulo} ${producto3.precio}
+  Marque 3 para ${producto3.articulo} ${producto3.precio} 
   Marque 4 para ${producto4.articulo} ${producto4.precio}`
 );
 
@@ -37,6 +37,11 @@ let total = 0;
 
 function postreElegido() {
   while (ingresado != "NO") {
+    console.log(
+      `${nombre}, Elegiste ${postresElegidos[ingresado - 1].articulo}`
+    );
+    total += postresElegidos[ingresado - 1].precioConIva();
+
     ingresado = prompt(
       `${nombre}, ¿quieres seguir eligiendo postres?, elige que otro postre sumas de lo contrario escribe NO
       Marque 1 para ${producto1.articulo} ${producto1.precio}
@@ -44,6 +49,9 @@ function postreElegido() {
       Marque 3 para ${producto3.articulo} ${producto3.precio}
       Marque 4 para ${producto4.articulo} ${producto4.precio}`
     );
+
+    console.log(`El total de tu factura es ${total}`);
   }
 }
+
 postreElegido();
